2b4e168798ca7515e000952ee98a748c8c916c64
sujitbidawe <bidawesujit@gmail.com>
2022-09-16 00:59:51 +0530

    ADDED: code and tests

---
+++ src/Greetings.test.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import Greetings from './Greetings';
+
+
+describe('Greetings', () => {
+    it('matches the snapshot when a name is passed', () => {
+        var view = render(
+            <Greetings name={'Mike'}/>
+        );
+        expect(view).toMatchSnapshot();
+    });
+
+    it('matches the snapshot when no name is passed', () => {
+        var view = render(
+            <Greetings />
+        );
+        expect(view).toMatchSnapshot();
+    });
+});
